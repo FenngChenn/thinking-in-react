329d9dca36a18eda49730025595b1803d1959474
FenngChenn <fcfight@163.com>
2016-12-24 16:22:08 +0800

    update stepone

--- component/stepOne.js
+++ component/stepOne.js
@@ -24,16 +24,30 @@ var SearchBar = React.createClass({
 
 var ProductTable = React.createClass({
   render:function(){
-    var row = [];
+    var rows = [];
     var lastCategory = null;
     this.props.products.forEach(function(product){
-      
+      if(product.category !== lastCategory){
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />)
+      lastCategory = product.category;
     });
     return(
-
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
     );
   }
 });
+
+var ProductCategoryRow
 var PRODUCTS = [
   {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
   {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
