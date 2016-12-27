210513297bfbefd11928c2c694c1b6fd9b657b56
FenngChenn <fcfight@163.com>
2016-12-27 23:35:40 +0800

    sure where is state

--- component/stepOne.js
+++ component/stepOne.js
@@ -1,9 +1,15 @@
 var FilterableProductTable = React.createClass({
+  getInitialState: function(){
+    return(
+      filterText: '',
+      inStockOnly: false
+    );
+  },
   render: function(){
     return(
       <div>
-        <SearchBar />
-        <ProductTable products={this.props.products} />
+        <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly} />
+        <ProductTable products={this.props.products}filterText={this.state.filterText} inStockOnly={this.state.inStockOnly}  />
       </div>
     );
   }
